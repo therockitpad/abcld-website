395ee9e585549cf2df507223a58e757a5c3c92fc
Archit Bhonsle <abhonsle2000@gmail.com>
2020-10-02 20:46:49 +0530

    home page text only carousel

--- prepareEmailFields.js
+++ prepareEmailFields.js
@@ -1,6 +1,5 @@
 module.exports = function prepareEmailFields(body) {
   const { name, email, services, message } = body;
-  console.log(body);
   if (!(name && email && services)) throw new Error('Empty fields');
   const subject = `For ${name}`;
   const text = `
